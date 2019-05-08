b6d26e66058dd11e21f7600f6c709471a7aaa1ce
git-your-hub <arischild1@gmail.com>
2019-05-08 07:21:54 -0400

    a start

---
+++ Connect4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Ari Schild on 5/8/19.
+//  Copyright © 2019 Ari Schild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
